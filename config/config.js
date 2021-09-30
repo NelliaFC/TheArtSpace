574f92d2ff5b6e36a5672747300fc9ff6329ae51
NeoNeonilla <nelliafleurova@gmail.com>
2021-09-30 11:29:19 -0500

    seeded the fake database, got it to work

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.git, {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
