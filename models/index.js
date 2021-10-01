d1b635d1a07cd5124ca368084210da55b4e00fdb
NeoNeonilla <nelliafleurova@gmail.com>
2021-10-01 12:40:03 -0500

    added to develop

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const Artist = require('./artist');
+const Artist = require('./Artist');
 const Post = require('./Post')
 const User = require('./User')
-const Comments = require('./comments')
+const Comments = require('./Comments')
 
 //create associations  to models
