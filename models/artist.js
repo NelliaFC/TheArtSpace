898d527c693b43f897720034566a1e09a942e3a4
NeoNeonilla <nelliafleurova@gmail.com>
2021-09-30 22:28:44 -0500

    models and seeds work

--- models/artist.js
+++ models/artist.js
@@ -2,3 +2,17 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/config.js');
 
 class Artist extends Model {}
+
+Artist.init ({
+
+name: {
+type: DataTypes.STRING,
+allowNull: false,
+validate: {len: [1]
+   }
+  }
+},
+{ sequelize }
+);
+
+module.export = Artist
---
+++ seeds/artist-seeds.js
@@ -0,0 +1,11 @@
+const Artist = require('../models/fakedatamodel.js');
+const sequelize = require('../config/config');
+
+const artistdata = [
+    {
+      title: 'Pachinko',
+      author: 'Min Jin Lee',
+      genre: 'Fiction',
+      pages: 496
+    },
+]
