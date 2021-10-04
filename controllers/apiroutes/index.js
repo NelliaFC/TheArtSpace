3978bb11fef98f1f0d38c45af5111799eb59ff5b
NeoNeonilla <nelliafleurova@gmail.com>
2021-10-04 12:36:07 -0500

    tiny changes

--- controllers/apiroutes/index.js
+++ controllers/apiroutes/index.js
@@ -1,1 +1,15 @@
 //consolidated api routes only from api routes AND add part of the endpoints
+//consolidated api routes only from api routes AND add part of the 
+const router = require('express').Router();
+
+const userRoute = require('./user-auth');
+const postRoute = require('./post-route');
+const commentRoute = require('./comments-route');
+const artistRoute = require('./artist-route');
+
+router.use('/user', userRoute);
+router.use('/post', postRoute);
+router.use('/comments', commentRoute);
+router.use('/artist', artistRoute);
+
+module.exports = router;
