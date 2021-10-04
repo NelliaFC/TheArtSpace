062c0997d438179fbabdf835a87329ece331a112
Waleed Baig <85601766+wtbaig@users.noreply.github.com>
2021-10-03 19:39:39 -0700

    Updated files for the Login forms and Signup Forms

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const sequelize = require("../config/config");
 const { Artist, Comment, Post, User } = require("../models");
 
-//Get all rounts from home page
+//Get all routes from home page
 
 router.get("/", (req, res) => {
     res.render('gallery')
--- server.js
+++ server.js
@@ -9,26 +9,26 @@ const PORT = process.env.PORT || 3001;
 
 //Helpers for Handlebars
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
-//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
 const sequelize = require("./config/config.js");
 
 
 
 
-// const sess = {
-//   secret:'',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new Sequelize.Store({
-//     db: sequelize
-//   })
-// }
+const sess = {
+    secret: '',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new Sequelize.Store({
+        db: sequelize
+    })
+}
 
-//app.use(session(sess));
+app.use(session(sess));
 
 
 
@@ -41,9 +41,4 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(require('./controllers'));
 
 //sync sequelize models to the database, then turn on the server
-sequelize.sync({force: false}).then(() => 
-  { app.listen(PORT, () => console.log(`Team cool server is up and running  on port ${PORT}!`))}
-)
-
-
-  
+sequelize.sync({ force: false }).then(() => { app.listen(PORT, () => console.log(`Team cool server is up and running  on port ${PORT}!`)) });
