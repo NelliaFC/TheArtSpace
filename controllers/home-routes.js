c77849c6a4eb2bd26a2157f072126183effc764b
NeoNeonilla <nelliafleurova@gmail.com>
2021-10-05 16:10:21 -0500

    controller home route created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,18 +3,77 @@ const router = require("express").Router();
 const sequelize = require("../config/config");
 const { Artist, Comments, Post, User } = require("../models");
 
-//Get all routes from home page
+ // GO TO DB and get all the Posts Post.findAll()
 
 router.get("/", (req, res) => {
+    Post.findAll({
+      include: [User],
+    })
+      .then((dbPostData) => {
+        const posts = dbPostData.map((post) => post.get({ plain: true }));
+  
+        res.render("all-posts", { posts });//change 'all-posts' ??
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  
 
-    // GO TO DB and get all the POsts Post.findAll()
+// router.get("/", (req, res) => {
 
-    var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
+//     // GO TO DB and get all the POsts Post.findAll()
 
-    res.render('gallery', { posts: fakePosts })
-})
+//     var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
+
+//     res.render('gallery', { posts: fakePosts })
+// })
+
+// router.get("/login", (req, res) => {
+//     res.render('login')
+// })
+
+// get single post
+router.get("/post/:id", (req, res) => {
+  Post.findByPk(req.params.id, {
+    include: [
+      User,
+      {
+        model: Comments,
+        include: [User],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+
+        res.render("single-post", { post });
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 router.get("/login", (req, res) => {
-    res.render('login')
-})
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("signup");
+});
+
 module.exports = router;
