b88eb46051493f73e85700c9f1a6ae39ad3f8c50
SpyceCodes <dubstepandponies95@gmail.com>
2021-10-06 16:07:51 -0400

    Update stylings

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,40 +1,21 @@
 //gallery route (main page) includes "comments route"
-const router = require("express").Router();
-const sequelize = require("../config/config");
-const { Artist, Comments, Post, User } = require("../models");
+const router = require('express').Router();
+const sequelize = require('../config/config');
+const { Artist, Comments, Post, User } = require('../models');
 
 //Get all routes from home page
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-    
-    });
+    const postData = await Post.findAll({});
     const posts = postData.map((post) => post.get({ plain: true }));
     res.render('gallery', { posts });
+    // res.render('newpost', { user_id: req.session.id });
   } catch (err) {
     res.status(500).json(err);
   }
 });
-// router.get("/", (req, res) => {
 
-//     // GO TO DB and get all the POsts Post.findAll()
-
-
-    // GO TO DB and get all the POsts Post.findAll()
-
-    var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
-
-    res.render('newpost' , {user_id : req.session.id })
-    //res.render('gallery', { posts: fakePosts })
-})
-=======
-//     //var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
-//   var fakePosts = Post.findAll()
-//     res.render('gallery', { posts: fakePosts })
-// })
-
-
-router.get("/login", (req, res) => {
-    res.render('login')
-})
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,17 +2,16 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./apiroutes');
-const userRoutes = require('./apiroutes/user-auth')
-    //then list out each route PER folder of API routes
+const userRoutes = require('./apiroutes/user-auth');
+//then list out each route PER folder of API routes
 const homeRoutes = require('./home-routes.js');
 const artistpageRoute = require('./artistpage-route');
 //const userprofileRoute = require('./user-profile-routes.js');
 
-
 router.use('/', homeRoutes);
-router.use('/api/users', userRoutes)
-    //router.use('/artistpage', artistpageRoutes);
+router.use('/api/users', userRoutes);
+//router.use('/artistpage', artistpageRoutes);
 router.use('/api', apiRoutes);
-    //router.use('/user', userprofileRoutes);
+//router.use('/user', userprofileRoutes);
 
 module.exports = router;
