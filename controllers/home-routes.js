9961c18b648aeec8bbbba1a7408e200c33fa0806
NeoNeonilla <85714258+NelliaFC@users.noreply.github.com>
2021-10-06 12:35:31 -0500

    Developer (#16)
    * updated Artist API route
    
    * Updated files for the Login forms and Signup Forms
    
    * homepage is created
    
    * Index.js API routes creted
    
    * controller routes update
    
    * Front end2 (#11)
    
    * New Updates
    
    * New Updates
    
    * User-profile-routes (#12)
    
    Co-authored-by: Waleed Baig <85601766+wtbaig@users.noreply.github.com>
    Co-authored-by: kcs111 <86818752+kcs111@users.noreply.github.com>

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,20 @@
 //gallery route (main page) includes "comments route"
 const router = require("express").Router();
 const sequelize = require("../config/config");
-const { Artist, Comment, Post, User } = require("../models");
+const { Artist, Comments, Post, User } = require("../models");
 
-//Get all rounts from home page
+//Get all routes from home page
 
 router.get("/", (req, res) => {
-    res.render('gallery')
+
+    // GO TO DB and get all the POsts Post.findAll()
+
+    var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
+
+    res.render('gallery', { posts: fakePosts })
 })
 
+router.get("/login", (req, res) => {
+    res.render('login')
+})
 module.exports = router;
