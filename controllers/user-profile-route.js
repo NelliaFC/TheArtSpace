98f9a6e2d1321a0f03dd5b036161c5a2f378c94e
Kcs111 <kcs111@txstate.edu>
2021-10-06 16:38:56 -0500

    Routes

--- controllers/user-profile-route.js
+++ controllers/user-profile-route.js
@@ -1,5 +1,18 @@
 //route to the user page with saved art. 
 const router = require ("express").Router();
+const { Model } = require("sequelize/types");
 const sequalize = require("../config/config");
 const { Artist, Comments, Post, User} = require("../models")
 
+// root route
+router.get ("/", (req, res) => {
+    var fakePosts = [{title: "First Post"}, {titel: "Second post"}]
+
+    res.render("gallery", { post: fakePosts})
+}
+);
+router.get("/login", (req, res) => {
+    res.render("login")
+})
+
+module.exports = routers;
