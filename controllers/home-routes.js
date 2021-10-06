7132810bc5c4caeea911dd75d2b99f44e581703e
NeoNeonilla <nelliafleurova@gmail.com>
2021-10-06 15:14:17 -0500

    waleeds changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -27,8 +27,7 @@ router.get('/', async (req, res) => {
 
     res.render('newpost' , {user_id : req.session.id })
     //res.render('gallery', { posts: fakePosts })
-})
-=======
+
 //     //var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
 //   var fakePosts = Post.findAll()
 //     res.render('gallery', { posts: fakePosts })
