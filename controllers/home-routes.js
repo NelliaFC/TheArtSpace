e633fc76e8e66f6891a9989b7816da9149ed3a4b
NeoNeonilla <nelliafleurova@gmail.com>
2021-10-06 13:27:37 -0500

    working homeroute without user_id assigned

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,15 +4,25 @@ const sequelize = require("../config/config");
 const { Artist, Comments, Post, User } = require("../models");
 
 //Get all routes from home page
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+    
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    res.render('gallery', { posts });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// router.get("/", (req, res) => {
 
-router.get("/", (req, res) => {
+//     // GO TO DB and get all the POsts Post.findAll()
 
-    // GO TO DB and get all the POsts Post.findAll()
-
-    var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
-
-    res.render('gallery', { posts: fakePosts })
-})
+//     //var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
+//   var fakePosts = Post.findAll()
+//     res.render('gallery', { posts: fakePosts })
+// })
 
 router.get("/login", (req, res) => {
     res.render('login')
