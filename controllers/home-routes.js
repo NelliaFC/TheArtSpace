91b4060b25af1222984fc8110e86d66d0d474ad8
Waleed Baig <85601766+wtbaig@users.noreply.github.com>
2021-10-10 13:59:20 -0700

    updated the home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -17,78 +17,17 @@ router.get('/', async(req, res) => {
         res.status(500).json(err);
     }
 });
-// router.get("/", (req, res) => {
 
-// router.get("/", (req, res) => {
-
-
-//     // GO TO DB and get all the POsts Post.findAll()
-
-//     var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
-
-
-//     res.render('gallery', { posts: fakePosts })
-//     res.render('newpost' , {user_id : req.session.id })
-//     //res.render('gallery', { posts: fakePosts })
-
-// })
-
-//     //var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
-//   var fakePosts = Post.findAll()
-//     res.render('gallery', { posts: fakePosts })
-// })
-
-
-
-
-router.get("/", (req, res) => {
-
-    // GO TO DB and get all the POsts Post.findAll()
-
-    var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
-
-    res.render('gallery', { posts: fakePosts })
-})
 router.get("/homepage", (req, res) => {
     res.render('homepage')
 })
-module.exports = router;
-
-
-// router.get("/", (req, res) => {
-
-//     // GO TO DB and get all the POsts Post.findAll()
-
-//     // var fakePosts = [{ title: 'First Post' }, { title: ' Second post' }]
-
-//     res.render('gallery', { posts: fakePosts })
-// })
-
-//Get all routes from home page
-router.get('/', async(req, res) => {
-    try {
-        const postData = await Post.findAll({});
-        const posts = postData.map((post) => post.get({ plain: true }));
-        res.render('gallery', { posts });
-        // res.render('newpost', { user_id: req.session.id });
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// router.get('/login', (req, res) => {
-//   res.render('login');
-// });
-
 router.get("/login", (req, res) => {
     res.render('login')
 })
 router.get("/signup", (req, res) => {
     res.render('signup')
 })
-router.get("/homepage", (req, res) => {
-    res.render('hompage')
-})
+
 router.get("/homepage", (req, res) => {
     res.render('logout')
 })
