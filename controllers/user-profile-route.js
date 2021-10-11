7dc6386c54685ccdff8021b9e54248ac8c7e2748
NeoNeonilla <nelliafleurova@gmail.com>
2021-10-10 20:50:17 -0500

    inserted last user controller route from Kyla

--- controllers/user-profile-route.js
+++ controllers/user-profile-route.js
@@ -5,8 +5,33 @@ const withAuth = require("../utils/auth");
 
 
 
-const router = require ("express").Router();
-const sequalize = require("../config/config");
-const { Artist, Comments, Post, User} = require("../models")
+const { any } = require("sequelize/types/lib/operators");
+const {User, Post, Comments} = require("../models/user");
+const router = require("./home-routes");
+
+router.get("/", (req, res)=> {
+    User.findAll({
+        attribute: {excluder:["password"]}
+    })
+    .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    });
+router.get("/:id", (req, res) => {
+    User.findOne({"userID": any})
+    .exec()
+    .then(doc => {
+        res.render("user.hbs", {
+            name: doc.name,
+            nick: doc.nickname
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+module.export = router;
 
 
