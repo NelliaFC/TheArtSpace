291f3cd5c0521e03f40d69399ea096a566061d09
Waleed Baig <85601766+wtbaig@users.noreply.github.com>
2021-10-12 13:49:40 -0700

    uodated the routes that were not the same as main branch

--- controllers/user-profile-route.js
+++ controllers/user-profile-route.js
@@ -5,8 +5,31 @@ const withAuth = require("../utils/auth");
 
 
 
-const router = require ("express").Router();
+const router = require("express").Router();
 const sequalize = require("../config/config");
-const { Artist, Comments, Post, User} = require("../models")
+const { Artist, Comments, Post, User } = require("../models")
 
-
+router.get("/", (req, res) => {
+    User.findAll({
+            attribute: { excluder: ["password"] }
+        })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+router.get("/:id", (req, res) => {
+    User.findOne({ "userID": any })
+        .exec()
+        .then(doc => {
+            res.render("user.hbs", {
+                name: doc.name,
+                nick: doc.nickname
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+});
+module.export = router;
